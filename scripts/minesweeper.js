1164f36e71675522b589304020556064f62d8840
PokeJofeJr4th <PokeJofeJr4th@gmail.com>
2024-07-12 21:09:48 -0400

    start games infrastructure, work on minesweeper

--- scripts/minesweeper.js
+++ scripts/minesweeper.js
@@ -1,15 +1,19 @@
 const map = [];
+const mapRevealed = [];
 var gameStarted = false;
 
 function resetBoard() {
+  gameStarted = false;
   let table_html = "";
   // fill in an empty map
   for (let i = 0; i < 10; i++) {
     map[i] = [];
+    mapRevealed[i] = [];
     table_html += `<tr id=\"${i} _\">`;
     for (let j = 0; j < 10; j++) {
       map[i][j] = Math.random() < 0.15;
-      table_html += `<th id=\"${i} ${j}\" onClick=\"handleClick(${i},${j})\" class=\"unknown\"></th>`;
+      mapRevealed[i][j] = false;
+      table_html += `<th id=\"${i} ${j}\" onClick=\"handleClick(event,${i},${j})\" class=\"unknown\"></th>`;
     }
     table_html += `</tr>`;
   }
@@ -20,22 +24,39 @@ window.onload = (_) => {
   resetBoard();
 };
 
-function handleClick(row, col) {
+function handleClick(event, row, col) {
   if (!gameStarted) {
     map_window(row, col, (r, c) => (map[r][c] = false));
     gameStarted = true;
   }
 
-  if (map[row][col]) {
-    window.alert("Kaboom :(");
-    resetBoard();
+  if (event.shiftKey && !mapRevealed[row][col]) {
+    document.getElementById(`${row} ${col}`).className = "flag";
   } else {
-    let i = 0;
-    map_window(row, col, (r, c) => {
-      if (map[r][c]) {
-        i += 1;
-      }
-    });
+    if (map[row][col]) {
+      window.alert("Kaboom :(");
+      resetBoard();
+    } else {
+      reveal(row, col);
+    }
+  }
+}
+
+function reveal(row, col) {
+  if (mapRevealed[row][col]) {
+    return;
+  }
+  mapRevealed[row][col] = true;
+  let i = 0;
+  map_window(row, col, (r, c) => {
+    if (map[r][c]) {
+      i += 1;
+    }
+  });
+  if (i == 0) {
+    document.getElementById(`${row} ${col}`).className = "empty";
+    map_window(row, col, reveal);
+  } else {
     document.getElementById(`${row} ${col}`).innerText = i;
   }
 }
